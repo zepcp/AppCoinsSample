0b604d1bfde0e8bd21c40b388db81a33bc0e13e7
José Pereira <vieirapereira@protonmail.com>
2020-08-05 14:49:47 +0100

    fix bug

--- contracts/AppCoinsIAB.sol
+++ contracts/AppCoinsIAB.sol
@@ -1,40 +1,8 @@
 pragma solidity >=0.4.25 <0.7.0;
 
 import { Shares } from "./lib/Shares.sol";
-
-contract AppCoins {
-    mapping (address => mapping (address => uint256)) public allowance;
-    function balanceOf (address _owner) public view returns (uint256);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (uint);
-}
-/**
-@title AppCoinsIAB Interface
-@author App Store Foundation
-@dev Base interface to implement In-app-billing functions.
-*/
-contract AppCoinsIABInterface {
-
-    /**
-    @notice Function to register a in-app-billing operation
-    @dev
-        Registers a in-app-billing operation with the needed information and transfers the correct
-        amount from the user to the developer and remaining parties.
-    @param _packageName Package name of the application from which the in-app-billing was generated
-    @param _sku Item id for the item bought inside the specified application
-    @param _amount Value (in wei) of AppCoins to be paid for the item
-    @param _addr_appc Address of the AppCoins (ERC-20) contract to be used
-    @param _dev Address of the application's developer
-    @param _appstore Address of the appstore to receive part of the share
-    @param _oem Address of the OEM to receive part of the share
-    @param _countryCode Country code of the country from which the transaction was issued
-    @return {"result" : "True if the transaction was successfull"}
-    */
-    function buy(
-        string memory _packageName, string memory _sku, uint256 _amount, address _addr_appc, address _dev,
-        address _appstore, address _oem, bytes2 _countryCode)
-        public
-        returns (bool result);
-}
+import { AppCoinsIABInterface } from "./lib/AppCoinsIABInterface.sol";
+import { AppCoins } from "./AppCoins.sol";
 
 
 contract AppCoinsIAB is AppCoinsIABInterface {
